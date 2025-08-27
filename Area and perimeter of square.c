15b1534bb6d907d7a2fa4c5ee7843eecbe212827
dhruvrajsinh1 <dhruvrajsinhg772@gmail.com>
2025-08-27 23:38:14 +0530

    Create Area and perimeter of square.c

---
+++ Area and perimeter of square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float L,area,perimeter;
+printf("enter the length of square");
+scanf("%f",&L);
+area=L*L;
+perimeter=4*L;
+printf("the area of square is %.2f\n",perimeter);
+return 0;
+}
