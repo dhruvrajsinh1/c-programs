d493ebbf746c5b630ae983724aa1e73046c6506b
dhruvrajsinh1 <dhruvrajsinhg772@gmail.com>
2025-08-27 23:26:23 +0530

    Create Dollar into pound.c

---
+++ Dollar into pound.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,c;
+prinf("ENTER DOLLAR:");
+scanf("%f",&a);
+b=a*48;
+c=b/70;
+prinf("%.3f DOLLAR IN pound IS %.3f",a,c);
+
+return 0;
+}
