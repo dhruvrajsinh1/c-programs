c23fbd8f003a9f3a3b61736a877de7dc8b029fc1
dhruvrajsinh1 <dhruvrajsinhg772@gmail.com>
2025-08-27 23:28:13 +0530

    Create kilo into gram.c

---
+++ kilo into gram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float gram;
+float kilogram;
+scanf("%f", &kilograms);
+prinf("Enter weigth in kilogram:");
+gram=kilogram * 1000.0;
+printf("%.2f kilograms is equal to %.2f gram.\n",gram,kilogram);
+return 0;
+}
