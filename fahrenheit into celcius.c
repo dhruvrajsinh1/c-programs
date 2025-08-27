68850e57b171cdd0e26c628b957735c00ff583e0
dhruvrajsinh1 <dhruvrajsinhg772@gmail.com>
2025-08-27 23:35:15 +0530

    Create fahrenheit into celcius.c

---
+++ fahrenheit into celcius.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float celcius;
+float fahrenheit;
+printf("Enter the temperature in fahrenheit:");
+scanf(%f,&fahrenheit);
+celuis=(5/9*fahrenheit)-32;
+
+printf("%.2f fahrenheit is eqauls to %.2f celcius.",celcius,fahrenheit);
+return 0;
+}
