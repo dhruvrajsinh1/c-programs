dee140fb19bec9c019e33be3c1fbab250ce9e21e
dhruvrajsinh1 <dhruvrajsinhg772@gmail.com>
2025-08-29 00:51:26 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int a, b, ans;
+
+printf("Enter value of a: ");
+scanf("%d", &a);
+
+printf("Enter value of b: ");
+scanf("%d", &b);
+ans = a * b;
+printf("ans (a * b) = %d\n", ans);
+return 0;
+}
